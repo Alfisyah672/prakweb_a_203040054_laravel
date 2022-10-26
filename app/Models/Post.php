5dda61edb677e398ea92ef78ade6c57192fc6aa9
AlfiS.R <alfisyahrin673@gmail.com>
2022-10-26 19:43:55 +0700

    Laravel Video 7

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,31 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Tulisan Pertama",
-            "slug" => "judul-tulisan-pertama",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dignissimos deserunt atque voluptatibus expedita cumque molestias! Eligendi et iusto minima sed sapiente quo tenetur vel doloribus repellat? Unde animi iste debitis consequuntur sint dignissimos incidunt tempore earum aspernatur repellat temporibus saepe neque aperiam soluta veritatis, nemo hic quos quam quis delectus aliquid libero velit? Delectus voluptatum cumque cum numquam consequuntur odit ducimus obcaecati ab! Excepturi veritatis, id culpa tempora voluptatem amet beatae illo vitae dicta eaque sequi corporis, itaque aut!"
-        ],
-        [
-            "title" => "Judul Tulisan Kedua",
-            "slug" => "judul-tulisan-kedua",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus, nisi odio. Corrupti architecto magnam dignissimos, et optio blanditiis, rerum non id quidem quo vel, aliquid voluptatum maiores. Blanditiis accusamus odit, laudantium voluptatibus officiis a ducimus perspiciatis neque dolorem, exercitationem molestiae nemo beatae voluptate, numquam odio quia suscipit at. Nobis, itaque nemo. Enim, autem unde quidem, tempora eligendi nesciunt odio blanditiis aliquid necessitatibus recusandae a, eum repudiandae! Minima voluptas, repellat nulla suscipit corrupti blanditiis! Temporibus optio possimus officiis! Reprehenderit sunt fugit voluptate minus quos, adipisci, fugiat aut voluptatem eligendi laudantium maxime iure quae harum nobis! Voluptatem ex minus non fuga repellendus!"
-        ],
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,10 @@
 @extends('layout.main')
 
 @section('container')
-    <article>
-        <h2 class="mb-5">{{ $post['title'] }}</h2>
-        <p>{{ $post['body'] }}</p>
-    </article>
+    <h2 class="mb-5">{{ $post->title }}</h2>
+
+    {!! $post->body !!}
+
 
     <a href="/posts">Back to Posts</a>
 @endsection
