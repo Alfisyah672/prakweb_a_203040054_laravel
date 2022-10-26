b909255b1d1fffe925ff8791ee2e3236e157468a
AlfiS.R <alfisyahrin673@gmail.com>
2022-10-26 21:19:11 +0700

    Laravel Video 8

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('container')
-    <h1 class="mb-5">Halaman Blog Post</h1>
+    <h1 class="mb-5">Post Category : {{ $category }}</h1>
     @foreach ($posts as $post)
         <article class="mb-5">
             <h2>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
     <h2 class="mb-5">{{ $post->title }}</h2>
 
+    <p>By. Alfi Syahrin in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
 
 
