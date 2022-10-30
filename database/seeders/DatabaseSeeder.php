4a1798beabcbcf6166c42683cba472276db111a8
AlfiS.R <alfisyahrin673@gmail.com>
2022-10-30 23:01:30 +0700

    Laravel Video 10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //\App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => 'Alfi Syahrin R',
-            'email' => 'alfisyahrin672@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
 
-        User::create([
-            'name' => 'Mamank Ginanjar',
-            'email' => 'mamank12@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Alfi Syahrin R',
+        //     'email' => 'alfisyahrin672@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        // User::create([
+        //     'name' => 'Mamank Ginanjar',
+        //     'email' => 'mamank12@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::created([
             'name' => 'Web Programming',
@@ -40,40 +42,42 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia quia sunt inventore vitae illum eius est quidem. Temporibus fugit excepturi omnis ratione amet necessitatibus atque in beatae voluptate vero! Laborum explicabo molestiae dolores odit minima reprehenderit animi, velit voluptatum accusantium neque ex necessitatibus nemo, atque dolorem, quidem expedita nulla maxime est architecto rem? A, vero quis, exercitationem sequi quisquam corrupti facere atque neque porro quam voluptates ut animi nihil iure consequuntur voluptatum ipsa aspernatur quidem illum! Quidem ex, veritatis placeat numquam aliquid culpa iusto? Quae corrupti optio ducimus dolor tempore dolore animi facilis eaque ad laboriosam. Inventore, deserunt quae.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia quia sunt inventore vitae illum eius est quidem. Temporibus fugit excepturi omnis ratione amet necessitatibus atque in beatae voluptate vero! Laborum explicabo molestiae dolores odit minima reprehenderit animi, velit voluptatum accusantium neque ex necessitatibus nemo, atque dolorem, quidem expedita nulla maxime est architecto rem? A, vero quis, exercitationem sequi quisquam corrupti facere atque neque porro quam voluptates ut animi nihil iure consequuntur voluptatum ipsa aspernatur quidem illum! Quidem ex, veritatis placeat numquam aliquid culpa iusto? Quae corrupti optio ducimus dolor tempore dolore animi facilis eaque ad laboriosam. Inventore, deserunt quae.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia quia sunt inventore vitae illum eius est quidem. Temporibus fugit excepturi omnis ratione amet necessitatibus atque in beatae voluptate vero! Laborum explicabo molestiae dolores odit minima reprehenderit animi, velit voluptatum accusantium neque ex necessitatibus nemo, atque dolorem, quidem expedita nulla maxime est architecto rem? A, vero quis, exercitationem sequi quisquam corrupti facere atque neque porro quam voluptates ut animi nihil iure consequuntur voluptatum ipsa aspernatur quidem illum! Quidem ex, veritatis placeat numquam aliquid culpa iusto? Quae corrupti optio ducimus dolor tempore dolore animi facilis eaque ad laboriosam. Inventore, deserunt quae.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia quia sunt inventore vitae illum eius est quidem. Temporibus fugit excepturi omnis ratione amet necessitatibus atque in beatae voluptate vero! Laborum explicabo molestiae dolores odit minima reprehenderit animi, velit voluptatum accusantium neque ex necessitatibus nemo, atque dolorem, quidem expedita nulla maxime est architecto rem? A, vero quis, exercitationem sequi quisquam corrupti facere atque neque porro quam voluptates ut animi nihil iure consequuntur voluptatum ipsa aspernatur quidem illum! Quidem ex, veritatis placeat numquam aliquid culpa iusto? Quae corrupti optio ducimus dolor tempore dolore animi facilis eaque ad laboriosam. Inventore, deserunt quae.',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia quia sunt inventore vitae illum eius est quidem. Temporibus fugit excepturi omnis ratione amet necessitatibus atque in beatae voluptate vero! Laborum explicabo molestiae dolores odit minima reprehenderit animi, velit voluptatum accusantium neque ex necessitatibus nemo, atque dolorem, quidem expedita nulla maxime est architecto rem? A, vero quis, exercitationem sequi quisquam corrupti facere atque neque porro quam voluptates ut animi nihil iure consequuntur voluptatum ipsa aspernatur quidem illum! Quidem ex, veritatis placeat numquam aliquid culpa iusto? Quae corrupti optio ducimus dolor tempore dolore animi facilis eaque ad laboriosam. Inventore, deserunt quae.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-Empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia quia sunt inventore vitae illum eius est quidem. Temporibus fugit excepturi omnis ratione amet necessitatibus atque in beatae voluptate vero! Laborum explicabo molestiae dolores odit minima reprehenderit animi, velit voluptatum accusantium neque ex necessitatibus nemo, atque dolorem, quidem expedita nulla maxime est architecto rem? A, vero quis, exercitationem sequi quisquam corrupti facere atque neque porro quam voluptates ut animi nihil iure consequuntur voluptatum ipsa aspernatur quidem illum! Quidem ex, veritatis placeat numquam aliquid culpa iusto? Quae corrupti optio ducimus dolor tempore dolore animi facilis eaque ad laboriosam. Inventore, deserunt quae.',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia quia sunt inventore vitae illum eius est quidem. Temporibus fugit excepturi omnis ratione amet necessitatibus atque in beatae voluptate vero! Laborum explicabo molestiae dolores odit minima reprehenderit animi, velit voluptatum accusantium neque ex necessitatibus nemo, atque dolorem, quidem expedita nulla maxime est architecto rem? A, vero quis, exercitationem sequi quisquam corrupti facere atque neque porro quam voluptates ut animi nihil iure consequuntur voluptatum ipsa aspernatur quidem illum! Quidem ex, veritatis placeat numquam aliquid culpa iusto? Quae corrupti optio ducimus dolor tempore dolore animi facilis eaque ad laboriosam. Inventore, deserunt quae.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-Empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo mollitia quia sunt inventore vitae illum eius est quidem. Temporibus fugit excepturi omnis ratione amet necessitatibus atque in beatae voluptate vero! Laborum explicabo molestiae dolores odit minima reprehenderit animi, velit voluptatum accusantium neque ex necessitatibus nemo, atque dolorem, quidem expedita nulla maxime est architecto rem? A, vero quis, exercitationem sequi quisquam corrupti facere atque neque porro quam voluptates ut animi nihil iure consequuntur voluptatum ipsa aspernatur quidem illum! Quidem ex, veritatis placeat numquam aliquid culpa iusto? Quae corrupti optio ducimus dolor tempore dolore animi facilis eaque ad laboriosam. Inventore, deserunt quae.',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <h2 class="mb-5">{{ $post->title }}</h2>
 
-    <p>By. <a href="#" class="text-decoration-none">{{ $post - user - name }}</a> in <a
+    <p>By. <a href="/authors/{{ $post->user->username }}" class="text-decoration-none">{{ $post - user - name }}</a> in <a
             href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
     {!! $post->body !!}
