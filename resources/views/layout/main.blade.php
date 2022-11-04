dcfaa1603fc78ce5e328d3b0383520794316a207
AlfiS.R <alfisyahrin673@gmail.com>
2022-11-04 18:59:10 +0700

    Laravel Video 14

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,10 +5,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>ALS | {{ $title }}</title>
 
+    {{-- Bootstrap Css --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    {{-- Bootstrap icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    {{-- my styles --}}
+    <link rel="stylesheet" href="/css/style.css">
+
+    <title>ALS | {{ $title }}</title>
 </head>
 
 <body>
