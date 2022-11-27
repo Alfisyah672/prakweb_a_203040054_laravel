96e0a2f15f019b61e01f66dc9ffaae4fc04dcd6c
AlfiS.R <alfisyahrin673@gmail.com>
2022-11-27 18:50:05 +0700

    Laravel Video 20

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,8 +7,12 @@
                 <h1 class="my-3">{{ $post->title }}</h1>
 
                 <a href="/dashboard/posts" class="btn btn-success my-2">Back To All Post</a>
-                <a href="" class="btn btn-warning my-2">Edit</a>
-                <a href="" class="btn btn-danger my-2">Delete</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning my-2">Edit</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger my-2" onclick="return confirm('Are you sure??')">Delete</button>
+                </form>
 
                 <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
                     alt="{{ $post->category->name }}" class="img-fluid">
